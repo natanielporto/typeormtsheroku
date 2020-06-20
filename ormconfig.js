d6f3bcc2515ee5026674d4f5cf8211b17f51ab8a
Nataniel Porto <natanielkporto@gmail.com>
2020-06-19 23:13:34 -0300

    All configs now fixed

--- ormconfig.js
+++ ormconfig.js
@@ -3,9 +3,9 @@ module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['dist/models/**/*.js'],
-  migrations: ['dist/migrations/**/*.js'],
+  migrations: ['dist/database/migrations/**/*.js'],
   cli: {
-    migrationsDir: ['src/migrations/'],
+    migrationsDir: ['src/database/migrations/'],
     entitiesDir: 'src/models',
   },
 };
